607001b61eb73cd93f08b97f65cc044f27168e45
M Natoli <natoli.missy@gmail.com>
2021-07-14 18:31:08 -0500

    updated index api routes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('pubic'));
 
-app.use(require('./routes'));
+app.use(require('./routes/api'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/thought-bubble', {
     useFindAndModify: false,
